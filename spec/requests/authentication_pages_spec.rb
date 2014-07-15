717ab2f78b4605e9fb73058014f720d8b97a1e4c
iivan <iivan@pitechnologies.ro>
2014-07-15 13:49:12 +0300

    chapter 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,6 +26,7 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
+      # why? before { sign_in user }
       before do
         fill_in "Email", with: user.email.upcase
         fill_in "Password", with: user.password
@@ -33,10 +34,14 @@ describe "Authentication" do
       end
 
       it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+
+
 
       describe "followed by signout" do
         before { click_link "Sign out" }
@@ -75,6 +80,10 @@ describe "Authentication" do
             before { patch user_path(user) }
             specify { expect(response).to redirect_to(signin_path) }
           end
+          describe "visiting the user index" do
+            before { visit users_path }
+            it { should have_title('Sign in') }
+          end
         end
       end
       describe "as wrong user" do
@@ -90,6 +99,18 @@ describe "Authentication" do
 
         describe "submitting a PATCH request to the Users#update action" do
           before { patch user_path(wrong_user) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+      end
+
+      describe "as non-admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:non_admin) { FactoryGirl.create(:user) }
+
+        before { sign_in non_admin, no_capybara: true }
+
+        describe "submitting a DELETE request to the Users#destroy action" do
+          before { delete user_path(user) }
           specify { expect(response).to redirect_to(root_url) }
         end
       end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,8 +3,61 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit users_path
+    end
 
-  describe "profile page" do
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+
+
+    it "should list each user" do
+      User.all.each do |user|
+        expect(page).to have_selector('li', text: user.name)
+      end
+    end
+    end
+
+    describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
     it { should have_content(user.name) }
@@ -36,7 +89,7 @@ describe "User pages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
